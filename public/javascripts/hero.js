27c942abcbcf2f9e780d6683c70b967335d633e2
david-gagne <dgagne813@gmail.com>
2018-06-19 22:54:14 -0400

    Added new logo, navigation bar, and registration buttons as façade to landing page.

--- public/javascripts/hero.js
+++ public/javascripts/hero.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     let hero = document.querySelector('div.hero-body');
+    let landingPageNavbar = document.querySelector('.hero .navbar');
+    
 
     hero.style.backgroundColor = "rgba(33, 33, 33, .7)";
+    landingPageNavbar.style.backgroundColor = "rgba(33, 33, 33, .7)";
 }
