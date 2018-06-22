057b7b8c76f53ee6d3b33ea6f949759e213d818b
david-gagne <dgagne813@gmail.com>
2018-06-22 17:44:45 -0400

    Added favicon and library route

--- app.js
+++ app.js
@@ -4,30 +4,45 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.static('views'));
 
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
     response.sendFile('index.html');
 });
 
-app.get("/libraries", function(request, response) {
+app.get("/libraries", function (request, response) {
 
     let options = {
         root: 'views',
         dotfiles: 'ignore'
-      };
+    };
 
-    response.sendFile('libraries.html', options, function(err) {
+    response.sendFile('libraries.html', options, function (err) {
         if (err) {
             console.log(err);
         } else {
-            console.log("Sent libraries.html!");
+            console.log("Sent libraries page!");
+        }
+    });
+
+});
+
+app.get("/libraries/:id", function (request, response) {
+
+    let options = {
+        root: 'views',
+        dotfiles: 'ignore'
+    };
+
+    response.sendFile('libraries.html', options, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Sent library page!");
         }
     });
 
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server started on port 3000!");
 });
-
-
